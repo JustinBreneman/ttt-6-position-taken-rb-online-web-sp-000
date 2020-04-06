a38556e00ede2ae20244601bebe447c0914ec619
Justin Breneman <justin.breneman@yahoo.com>
2020-04-06 08:31:56 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,4 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] == ("X" || "O")
-    true
-  elsif board[index] != ("X" || "O")
-    false
-  end
+  board[index] == " "
 end
